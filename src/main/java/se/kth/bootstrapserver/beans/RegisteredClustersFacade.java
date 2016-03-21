bd055fab7a930b40bd3a0d63191ec9ebce89eb4e
jsvhqr <jsvhqr@yahoo.com>
2016-03-21 12:41:37 +0100

    updates

--- src/main/java/se/kth/bootstrapserver/beans/RegisteredClustersFacade.java
+++ src/main/java/se/kth/bootstrapserver/beans/RegisteredClustersFacade.java
@@ -5,9 +5,11 @@
  */
 package se.kth.bootstrapserver.beans;
 
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import se.kth.bootstrapserver.entities.RegisteredClusters;
 
 /**
@@ -19,6 +21,32 @@ public class RegisteredClustersFacade extends AbstractFacade<RegisteredClusters>
 
     @PersistenceContext(unitName = "hops-sitePU")
     private EntityManager em;
+    
+    @Override
+    public List<RegisteredClusters> findAll(){
+        
+        TypedQuery<RegisteredClusters> query = em.createNamedQuery("RegisteredClusters.findAll",RegisteredClusters.class);
+        
+        return query.getResultList();
+        
+    }
+    
+    public void registerCluster(String name, String restendpoint, String email, String cert, boolean udpsupport){
+        
+        RegisteredClusters rc = new RegisteredClusters(name,restendpoint,email,cert,udpsupport);
+        
+        em.persist(rc);
+        
+        
+    }
+    
+    
+    public Boolean clusterExists(String name){
+        
+        return em.find(RegisteredClusters.class, name) != null;
+        
+    }
+    
     
     
 
--- src/main/java/se/kth/bootstrapserver/rest/ClustersPingService.java
+++ src/main/java/se/kth/bootstrapserver/rest/ClustersPingService.java
@@ -5,31 +5,50 @@
  */
 package se.kth.bootstrapserver.rest;
 
+import java.util.List;
+import javax.ejb.EJB;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.Produces;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
+import se.kth.bootstrapserver.beans.NoCacheResponse;
+import se.kth.bootstrapserver.beans.RegisteredClustersFacade;
+import se.kth.bootstrapserver.entities.RegisteredClusters;
+
 /**
  *
  * @author jsvhqr
  */
-
 @Path("/available_clusters")
 @Produces(MediaType.APPLICATION_JSON)
 public class ClustersPingService {
-    
-    
+
+    @EJB
+    RegisteredClustersFacade registerdClustersFacade;
+
+    @EJB
+    NoCacheResponse noCacheResponse;
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response ping(@PathParam("name") String name,@PathParam("restendpoint") String rep, @PathParam("email") String mail, @PathParam("cert") String cert, @PathParam("udp_support") Boolean udps){
-        
-        
-        
-        
-        
-        return null;
+    public Response ping(@PathParam("name") String name, @PathParam("restendpoint") String rep, @PathParam("email") String mail, @PathParam("cert") String cert, @PathParam("udp_support") Boolean udps) {
+
+        List<RegisteredClusters> clusters;
+
+        if (registerdClustersFacade.clusterExists(name)) {
+
+            clusters = registerdClustersFacade.findAll();
+
+        } else {
+            registerdClustersFacade.registerCluster(name, rep, mail, cert, udps);
+            clusters = registerdClustersFacade.findAll();
+
+        }
+
+        return noCacheResponse.getNoCacheResponseBuilder(Response.Status.OK).entity(
+                clusters).build();
     }
-    
+
 }
