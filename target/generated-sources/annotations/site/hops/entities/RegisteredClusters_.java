1b1e4117e6e4428d088c759160a42fe6a6a7d349
jsvhqr <jsvhqr@yahoo.com>
2016-03-23 20:14:49 +0000

    added test, not done

--- target/generated-sources/annotations/site/hops/entities/RegisteredClusters_.java
+++ target/generated-sources/annotations/site/hops/entities/RegisteredClusters_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-03-23T19:54:05")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-03-23T20:14:29")
 @StaticMetamodel(RegisteredClusters.class)
 public class RegisteredClusters_ { 
 
