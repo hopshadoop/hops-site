0114831ad8df062a4511058542460066a5c6e707
Johan Svedlund Nordström <jsvhqr@yahoo.com>
2016-06-11 18:07:17 +0200

    udpates

--- target/generated-sources/annotations/site/hops/entities/PopularDatasets_.java
+++ target/generated-sources/annotations/site/hops/entities/PopularDatasets_.java
@@ -4,15 +4,15 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-06-10T11:40:13")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-06-11T15:07:18")
 @StaticMetamodel(PopularDatasets.class)
 public class PopularDatasets_ { 
 
-    public static volatile SingularAttribute<PopularDatasets, Integer> files;
+    public static volatile SingularAttribute<PopularDatasets, Integer> size;
+    public static volatile SingularAttribute<PopularDatasets, Integer> seeds;
     public static volatile SingularAttribute<PopularDatasets, Integer> leeches;
     public static volatile SingularAttribute<PopularDatasets, String> name;
     public static volatile SingularAttribute<PopularDatasets, String> datasetId;
-    public static volatile SingularAttribute<PopularDatasets, Integer> seeds;
-    public static volatile SingularAttribute<PopularDatasets, Integer> size;
+    public static volatile SingularAttribute<PopularDatasets, Integer> files;
 
 }
--- target/generated-sources/annotations/site/hops/entities/RegisteredClusters_.java
+++ target/generated-sources/annotations/site/hops/entities/RegisteredClusters_.java
@@ -4,17 +4,17 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-06-10T11:40:13")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-06-11T15:07:18")
 @StaticMetamodel(RegisteredClusters.class)
 public class RegisteredClusters_ { 
 
-    public static volatile SingularAttribute<RegisteredClusters, String> dateRegistered;
-    public static volatile SingularAttribute<RegisteredClusters, Long> heartbeatsMissed;
-    public static volatile SingularAttribute<RegisteredClusters, String> cert;
-    public static volatile SingularAttribute<RegisteredClusters, String> email;
-    public static volatile SingularAttribute<RegisteredClusters, String> dateLastPing;
     public static volatile SingularAttribute<RegisteredClusters, String> gvodEndpoint;
     public static volatile SingularAttribute<RegisteredClusters, String> searchEndpoint;
+    public static volatile SingularAttribute<RegisteredClusters, Long> heartbeatsMissed;
+    public static volatile SingularAttribute<RegisteredClusters, String> dateLastPing;
+    public static volatile SingularAttribute<RegisteredClusters, String> cert;
     public static volatile SingularAttribute<RegisteredClusters, String> clusterId;
+    public static volatile SingularAttribute<RegisteredClusters, String> email;
+    public static volatile SingularAttribute<RegisteredClusters, String> dateRegistered;
 
 }
