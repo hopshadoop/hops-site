b56bf3c6399de7cc429c380861e447af1910148e
jsvhqr <jsvhqr@yahoo.com>
2016-06-14 16:47:13 +0200

    updates

--- target/generated-sources/annotations/site/hops/entities/PopularDatasets_.java
+++ target/generated-sources/annotations/site/hops/entities/PopularDatasets_.java
@@ -4,15 +4,15 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-06-11T15:07:18")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-06-14T12:09:03")
 @StaticMetamodel(PopularDatasets.class)
 public class PopularDatasets_ { 
 
-    public static volatile SingularAttribute<PopularDatasets, Integer> size;
-    public static volatile SingularAttribute<PopularDatasets, Integer> seeds;
+    public static volatile SingularAttribute<PopularDatasets, Integer> files;
     public static volatile SingularAttribute<PopularDatasets, Integer> leeches;
     public static volatile SingularAttribute<PopularDatasets, String> name;
     public static volatile SingularAttribute<PopularDatasets, String> datasetId;
-    public static volatile SingularAttribute<PopularDatasets, Integer> files;
+    public static volatile SingularAttribute<PopularDatasets, Integer> seeds;
+    public static volatile SingularAttribute<PopularDatasets, Integer> size;
 
 }
