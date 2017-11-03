c75035136f4660eaedd3fb44d3d753a992266428
Alex Ormenisan <ormenisan.adrian@gmail.com>
2017-11-03 16:04:29 +0100

    fix

--- src/main/java/io/hops/site/util/SecurityHelper.java
+++ src/main/java/io/hops/site/util/SecurityHelper.java
@@ -4,10 +4,10 @@ import javax.ws.rs.core.SecurityContext;
 
 public class SecurityHelper {
   public static String getClusterRole(SecurityContext sc) {
-    if (sc.isUserInRole("clusters")) {
-      return "clusters";
-    } else if (sc.isUserInRole("admin")) {
+    if (sc.isUserInRole("admin")) {
       return "admin";
+    } else if (sc.isUserInRole("clusters")) {
+      return "clusters";
     } else {
       return "none";
     }
